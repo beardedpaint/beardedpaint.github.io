0352d6b1491b21f314f7895f3da8dafb3165594e
jacobstarnes <southplainssolutions@gmail.com>
2020-07-12 09:57:49 -0500

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('nav-bar-links')[0];
+const navbarLinks = document.getElementsByClassName('navbarlinks')[0];
 
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active')
